446b1b6d0ab1810ba8ac860d5265fdf175ad9235
juliomatcom <juliomatcom@gmail.com>
2017-06-04 14:18:59 +0200

    fix(gh repo)

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
       <div className="container App">
         <h1>
           GitHub contributors world map
-          <a href="https://github.com/53Lab/github-contributors-map-web" target="_blank">
+          <a href="https://github.com/53Lab/contributors-world-map-web" target="_blank">
             <img className="help-icon" title="See more information about this."
               src="./imgs/1496417895_Info_Circle_Symbol_Information_Letter.png" />
           </a>
@@ -23,12 +23,12 @@ class App extends Component {
 
         <div className="footer">
           <iframe 
-            src="https://ghbtns.com/github-btn.html?user=53Lab&repo=github-contributors-map-web&type=star&count=true&size=large"
+            src="https://ghbtns.com/github-btn.html?user=53Lab&repo=contributors-world-map-web&type=star&count=true&size=large"
             frameBorder="0" scrolling="0" width="160px" height="30px">
           </iframe>
 
           <iframe 
-            src="https://ghbtns.com/github-btn.html?user=53Lab&repo=github-contributors-map-web&type=fork&count=true&size=large"
+            src="https://ghbtns.com/github-btn.html?user=53Lab&repo=contributors-world-map-web&type=fork&count=true&size=large"
             frameBorder="0" scrolling="0" width="158px" height="30px">
           </iframe>
           <p>Say hi! <a href="https://twitter.com/juliomatcom">@juliomatcom</a></p>
