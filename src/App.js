c50c5529d759e40d5b575fcaff486bc6e44cabe6
juliomatcom <juliomatcom@gmail.com>
2017-06-20 23:05:28 +0200

    feat(use ContributorsMap component)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 import RepoForm from './components/RepoForm.js'
-import MapElem from './components/Map.js'
+import ContributorsMap from './components/ContributorsMap.js'
 
 class App extends Component {
   render () {
@@ -9,25 +9,26 @@ class App extends Component {
       <div className="container App">
         <h1>
           GitHub contributors world map
-          <a href="https://github.com/53Lab/contributors-world-map-web" target="_blank">
+          <a href="https://github.com/53Lab/contributors-world-map-web" target="_blank" rel="noopener noreferrer">
             <img className="help-icon" title="See more information about this."
+              alt="info"
               src="./imgs/1496417895_Info_Circle_Symbol_Information_Letter.png" />
           </a>
         </h1>
         <h6>Choose a <strong>repository</strong> and watch its contributors <u>locations</u> on Map</h6>
-        <RepoForm
-          phText=":user/:repository"
-          event="repoUpdated">
-        </RepoForm>
-        <MapElem form="repoForm" event="repoUpdated"></MapElem>
+
+        <RepoForm phText=":user/:repository" event="repoUpdated"></RepoForm>
+        <ContributorsMap form="repoForm" event="repoUpdated"></ContributorsMap>
 
         <div className="footer">
-          <iframe 
+          <iframe
+            title="stars"
             src="https://ghbtns.com/github-btn.html?user=53Lab&repo=contributors-world-map-web&type=star&count=true&size=large"
             frameBorder="0" scrolling="0" width="160px" height="30px">
           </iframe>
 
-          <iframe 
+          <iframe
+            title="forks"
             src="https://ghbtns.com/github-btn.html?user=53Lab&repo=contributors-world-map-web&type=fork&count=true&size=large"
             frameBorder="0" scrolling="0" width="158px" height="30px">
           </iframe>
